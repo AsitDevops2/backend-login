be744ff5b46d01ba305a20cffb58163812bb1c9e
Swathi Kompella <swathi.kompella@jadeglobal.com>
2021-05-24 15:33:55 +0530

    external tomcat

--- src/main/java/com/raley/Application.java
+++ src/main/java/com/raley/Application.java
@@ -2,6 +2,8 @@ package com.raley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -20,6 +22,13 @@ public class Application {
     @Bean
     public RestTemplate getRestTemplate() {
     	return new RestTemplate();
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+		return builder.sources(Application.class);
+    	
     }
 
 }
